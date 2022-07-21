13b399c936b747cf9fdb759c204c4fb7eb4b23c5
homemix <kenmutati@gmail.com>
2022-07-21 17:19:39 +0300

    0. Regex-ing - use re.sub

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3
+"""
+re sub for personal data
+"""
+import re
+
+
 def filter_datum(fields: list[str],
                  redaction: str,
                  message: str, separator: str) -> str:
     """
     Filter a message based on a list of fields and a redaction string.
     """
-    fields_dict = {}
-    message_list = message.split(separator)
-    for msg in message_list:
-        if msg != '':
-            key, value = msg.split('=')
-            fields_dict[key] = value
-            if key in fields:
-                fields_dict[key] = redaction
-    for key, value in fields_dict.items():
-        print(f'{key}={value}', end=';')
+    for f in fields:
+        message = re.sub(f'{f}=.*?{separator}',
+                         f'{f}={redaction}{separator}', message)
+    return message
