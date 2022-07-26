8e8267748a2b1d07c53bc0312c21846815b9b71f
homemix <kenmutati@gmail.com>
2022-07-26 10:40:25 +0300

    6. Basic auth

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+A basic auth class implementation
+"""
+from .auth import Auth
+
+
+class BasicAuth(Auth):
+    """
+    a Basic auth class implementation
+    """
+    pass
