16a3da2dada008355ac23f9f9734d212ab2e8e48
homemix <kenmutati@gmail.com>
2022-07-26 12:04:31 +0300

    10. Basic - User object

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,10 +3,11 @@
 A basic auth class implementation
 """
 from base64 import b64decode
-from typing import TypeVar
+
 
 from .auth import Auth
 from api.models.user import User
+from typing import TypeVar
 
 
 class BasicAuth(Auth):
