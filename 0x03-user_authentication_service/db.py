7a80e05654f9af0ceb5fa75574b11a50aa175134
homemix <kenmutati@gmail.com>
2022-08-02 15:49:19 +0300

    2. Find user fix error

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -39,7 +39,7 @@ class DB:
 
         return user
 
-    def find_user_by(self, **kwargs):
+    def find_user_by(self, **kwargs)-> User:
         """ Finds user by given parameters
         Return: User Object
         """
