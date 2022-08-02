aa48caea1c134239a753c5e869cb376e4d02dfe2
homemix <kenmutati@gmail.com>
2022-08-02 16:04:37 +0300

    4. Hash password

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Auth module
+"""
+
+from bcrypt import hashpw, gensalt
+
+
+def _hash_password(password):
+    """
+    Hash a password
+    :param password:
+    :return:
+    """
+    return hashpw(password.encode('utf-8'), gensalt())
