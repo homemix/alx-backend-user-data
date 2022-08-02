35e0c5c9deaa5c249b03c046846db1e2fe4a6052
homemix <kenmutati@gmail.com>
2022-08-02 15:43:52 +0300

    2. Find user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 """DB module
 """
+from requests import session
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from sqlalchemy.exc import NoResultFound, InvalidRequestError
-
-from user import Base, User
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
+from user import User, Base
 
 
 class DB:
