2ff279774ed245007e16f77f0a552dd6c6673f4b
homemix <kenmutati@gmail.com>
2022-08-02 16:24:19 +0300

    5. Register user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -4,6 +4,8 @@ Auth module
 """
 
 from bcrypt import hashpw, gensalt
+from db import DB
+from user import User
 
 
 def _hash_password(password: str) -> bytes:
@@ -13,3 +15,19 @@ def _hash_password(password: str) -> bytes:
     :return:
     """
     return hashpw(password.encode('utf-8'), gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        user = self._db.find_user_by(email=email)
+        if user:
+            raise ValueError(f'user {email} already exists')
+        pwd_hash = _hash_password(password)
+        self._db.add_user(email=email, hashed_password=pwd_hash)
+        return user
