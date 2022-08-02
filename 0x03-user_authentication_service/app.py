7dd13c0930d2947cc4c6e9a6934aec23ea378de0
homemix <kenmutati@gmail.com>
2022-08-02 16:41:42 +0300

    6. Basic Flask app

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+A flask application
+"""
+
+from flask import Flask,jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    """
+    return welcome message
+    :return:
+    """
+    return jsonify({"message": "Bienvenue"})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
